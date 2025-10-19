5d047e8f70ebd040e5f09ab8ad1bc49eeeb07cf5
Sand2159 <s_sama21@hotmail.com>
2025-10-18 21:59:40 -0600

    Delete Registro.php

--- Registro.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nombre = $_POST["nombre"];
-$telefono = $_POST["telefono"];
-
-echo "Los datos insertados son: " . $nombre . " y el telefono " . $telefono;
-
-echo "<br><a href=Registro.html>Página Principal</a>";
-?>
