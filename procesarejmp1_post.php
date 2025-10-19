08842f2c339b7d02a1e49f12c3a092a228e35549
Sand2159 <s_sama21@hotmail.com>
2025-10-18 21:57:32 -0600

    Delete procesarejmp1_post.php

--- procesarejmp1_post.php
+++
@@ -1,9 +0,0 @@
-Hola
-<?php echo $_POST["name"]; ?><br>
-Tu email es: <?php echo $_POST["email"]; ?><br>
-Tu edad es: <?php echo $_POST["edad"]; ?><br>
-Tu direccion es: <?php echo $_POST["direccion"]; ?><br>
-Tu deporte favorito es: <?php echo $_POST["deporte"]; ?><br>
-Tu color favorito es: <?php echo $_POST["color"]; ?><br>
-Tu pasatiempo favorito: <?php echo $_POST["pasatiempo"]; ?><br>
-Máximo grado de estudios: <?php echo $_POST["estudios"]; ?><br>
